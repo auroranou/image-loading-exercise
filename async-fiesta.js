6c3862b92ec166d7ee575d6737b3e3db95fe8f31
Aurora Nou <auroranou@gmail.com>
2018-10-08 12:11:10 -0400

    load images in batches using async / await

--- async-fiesta.js
+++ async-fiesta.js
@@ -1,20 +1,48 @@
 $(() => {
-
   const $bucket = $('.photo-bucket');
   const draw = img => $bucket.append(img);
+
+  // Variable for tracking whether to keep loading images or not
+  let loadingAllowed = true;
 
   // NOTE: The height and width variables can be changed to fetch different sized images.
   const getImageUrl = id => `https://process.fs.grailed.com/AJdAgnqCST4iPtnUxiGtTz/cache=expiry:max/rotate=deg:exif/rotate=deg:0/resize=width:30,height:30,fit:crop/output=format:jpg,quality:95/compress/${id}`;
 
   const startLoading = () => {
-    // TODO: Implement me.
-    console.log('Start!');
+    const batches = IMAGE_IDS.reduce((memo, curr, currIdx) => {
+      if (currIdx % 5 === 0) {
+        memo[currIdx / 5] = [curr];
+      } else {
+        const idx = Math.floor(currIdx / 5);
+        memo[idx].push(curr);
+      }
+      return memo;
+    }, []);
+
+    batches.map(batch => processImageBatch(batch));
+    console.log('Images loaded successfully');
   };
 
   const stopLoading = () => {
     // TODO: Implement me.
     console.log('Stop!');
+    loadingAllowed = false;
   };
+
+  function fetchImage(imgId) {
+    return new Promise((resolve, reject) => {
+      let img = new Image();
+      img.onload = () => resolve(img);
+      img.onerror = reject;
+      img.src = getImageUrl(imgId);
+    });
+  }
+
+  async function processImageBatch(batch) {
+    let batchPromises = batch.map(imgId => fetchImage(imgId));
+    const results = await Promise.all(batchPromises);
+    results.forEach(img => draw(img));
+  }
 
   $('button.start').on('click', startLoading);
   $('button.stop').on('click', stopLoading);
